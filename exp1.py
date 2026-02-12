aa2b5bc51c956a28829647396783a17c969c7b19
vaishnavikadam3280-sys <vaishnavikadam3280@gmail.com>
2026-02-12 11:16:32 +0530

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  5 11:28:23 2026
+
+@author: User
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  5 11:20:18 2026
+
+@author: User
+"""
+
+name = input ("Enter your Name: ")
+age = input ("Enter your age: ")
+division = input ("Enter your division: ")
+college = input ("Enter your college: ")
+print("\n-----student information-----")
+
+print("Name:", name)
+print("Age:", age)
+print("Division:", division)
+print("College:", college)
